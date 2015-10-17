a8384e7dd541c999f5da4044dc91493af08085f4
Ryan Kerr <hiroshikerr@gmail.com>
2015-10-16 22:07:45 -0400

    Forgot to add simple FileException class

---
+++ swiftdrive/Root/sd/swiftglobal/rk/expt/FileException.java
@@ -0,0 +1,9 @@
+package sd.swiftglobal.rk.expt;
+
+public class FileException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public FileException(String msg) {
+		super(msg);
+	}
+}
