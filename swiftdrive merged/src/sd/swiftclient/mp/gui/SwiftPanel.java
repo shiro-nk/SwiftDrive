4c232cbde33d4ee150e897687a97a891429acb44
Ryan Kerr <hiroshikerr@gmail.com>
2015-11-21 15:35:21 -0500

    Removed cached merged directory

--- swiftdrive merged/src/sd/swiftclient/mp/gui/SwiftPanel.java
+++
@@ -1,7 +0,0 @@
-package sd.swiftclient.mp.gui;
-
-import javax.swing.JPanel;
-
-public interface SwiftPanel {
-    public JPanel getPanel();
-}
--- swiftdrive merged/src/sd/swiftclient/mp/gui/Test.java
+++
@@ -1,9 +0,0 @@
-package sd.swiftclient.mp.gui;
-
-import sd.swiftglobal.rk.SystemTest;
-
-public class Test {
-	public static void main(String[] args) {
-		new SystemTest(true);
-	}
-}
